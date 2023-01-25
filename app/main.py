f5093dcd9bb662422f852c57302ef6ca8239ab9f
mellos14 <fejifo@gmail.com>
2023-01-26 00:30:59 +0100

    Pandas implementado

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 # Dualidad de los modulos
@@ -12,15 +13,12 @@ import charts
 
 def run():
 
-    data = read_csv.read_csv("./data.csv")
+    # country = input("type country --> ")
+    # data = list(filter(lambda item: item['Continent']== 'South America',data))
 
-    country = input("type country --> ")
-    data = list(filter(lambda item: item['Continent']== 'South America',data))
-    
-    countries = list(map(lambda item: item["Country/Territory"], data))
-    population = list(map(lambda item: float(item["World Population Percentage"]), data))
-    new_list = list(zip(countries,population))
-    charts.generate_pie_chart(countries,population)
+    # countries = list(map(lambda item: item["Country/Territory"], data))
+    # population = list(map(lambda item: float(item["World Population Percentage"]), data))
+    # new_list = list(zip(countries,population))
     # countrys_v1 = []
     # population_v1 = []
     # for x,y in new_list:
@@ -30,14 +28,22 @@ def run():
     #     print(countrys_v1)
     #     print(population_v1)
     # charts.generate_pie_chart(countrys_v1,population_v1)
-    result = utils.population_by_country(data, country)
+    data = read_csv.read_csv("./data.csv")
+    df = pd.read_csv('./data.csv')
+    df = df[df['Continent']== 'Africa']
+    country = input("type country --> ")
+    countries = df['Country/Territory'].values
+    population = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries,population)
     
+    result = utils.population_by_country(data, country)
+
     if len(result) > 0:
         country = result[0]
         labels, values = utils.get_population(country)
         charts.generate_bar_chart(country['Country/Territory'],labels, values)
 
-        
+
 
 
 if __name__ == "__main__":
